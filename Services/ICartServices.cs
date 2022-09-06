1fc0ed7d636dfaabb3e067878c482c4ac9722853
Addison Nugent <112973411+addisonnugent@users.noreply.github.com>
2022-09-06 19:53:24 -0400

    Create ICartServices.cs

---
+++ Services/ICartServices.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Library.MobileEcommerce.Services
+{
+	public interface ICartServices
+  {
+        public List<CartService> SavedCartsList { get; }
+
+        ObservableCollection<Item> GetCartProds();
+        void SaveCart(ObservableCollection<Item> carts);
+
+        void AddToCart(Product myprod);
+
+        void RemoveFromCart(int id);
+
+        void Clear();
+   }
+}
